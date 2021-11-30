cf6689538818a21576f32ac23a93b3dbcfc44fa0
Wiam <wiamnasr@gmail.com>
2021-11-30 09:58:51 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,13 +34,6 @@ function App() {
         }}
       />
 
-      <Rating>0</Rating>
-      <Rating>1.49</Rating>
-      <Rating>1.5</Rating>
-      <Rating>3</Rating>
-      <Rating>4</Rating>
-      <Rating>5</Rating>
-
       <CreditCard
         type="Visa"
         number="0123456789018845"
